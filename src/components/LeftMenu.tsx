3cfb93bef2dcf2fa9817c69129f94e5ed777d4ca
maxim <maxim.bleeker@gmail.com>
2022-01-22 20:46:47 -0600

    added faux data model

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useWindowDimensions } from "../hooks/useWindowDimensions";
+import Category from "../models/Category";
+import { getCategories } from "../services/DataService";
 
 export default function LeftMenu() {
   const { width } = useWindowDimensions();
+  const [categories, setCategories] = useState<JSX.Element>(
+    <div>Left Menu</div>
+  );
+
+  useEffect(function () {
+    try {
+      getCategories().then(function (categories: Array<Category>) {
+        const cats = categories.map(function (cat) {
+          return <li key={cat.id}>{cat.name}</li>;
+        });
+        setCategories(<ul className="category">{cats}</ul>);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   let returned =
-    width <= 768 ? null : <div className="leftmenu">Left Menu</div>;
+    width <= 768 ? null : <div className="leftmenu">{categories}</div>;
   return returned;
 }
